6a6e2f0c159507d0c33c80de3dea845a4f03d74b
abdoachhoubi <aachhoub@gmail.com>
2023-10-26 19:02:12 +0100

    Added more Screens

--- whispers/App.js
+++ whispers/App.js
@@ -3,14 +3,26 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Splash from "./screens/Splash";
 import Welcome from "./screens/Welcome";
+import Home from "./screens/Home";
+
 import Signin from "./screens/auth/Signin";
 import Signup from "./screens/auth/Signup";
 import Pass from "./screens/auth/Pass";
 import Email from "./screens/auth/Email";
-import Home from "./screens/home/Home";
-import Comments from "./screens/home/Comments";
-import Replies from "./screens/home/Replies";
-import Report from "./screens/home/Report";
+
+import Comments from "./screens/feed/Comments";
+import Replies from "./screens/feed/Replies";
+import Report from "./screens/feed/Report";
+import Post from "./screens/feed/Post";
+
+import Chat from "./screens/chat/Chat";
+
+import Account from "./screens/settings/Account";
+import Notifications from "./screens/settings/Notifications";
+import Bug from "./screens/settings/Bug";
+import Contact from "./screens/settings/Contact";
+import DeleteAcc from "./screens/settings/DeleteAcc";
+import RestorPass from "./screens/settings/RestorPass";
 
 import {
   useFonts,
@@ -60,61 +72,108 @@ export default function App() {
             component={Splash}
             options={{ headerShown: false }}
           />
-		  
+
           <Stack.Screen
             name="Welcome"
             component={Welcome}
             options={{ headerShown: false }}
           />
-		  
+
           <Stack.Screen
             name="Signin"
             component={Signin}
             options={{ headerShown: false }}
           />
-		  
+
           <Stack.Screen
             name="Signup"
             component={Signup}
             options={{ headerShown: false }}
           />
-		  
+
           <Stack.Screen
             name="Pass"
             component={Pass}
             options={{ headerShown: false }}
           />
-		  
+
           <Stack.Screen
             name="Email"
             component={Email}
             options={{ headerShown: false }}
           />
-		  
+
           <Stack.Screen
             name="Home"
             component={Home}
             options={{ headerShown: false }}
           />
-		  
+
+          <Stack.Screen
+            name="Post"
+            component={Post}
+            options={{ headerShown: false }}
+          />
+
           <Stack.Screen
             name="Comments"
             component={Comments}
             options={{ headerShown: false }}
           />
-		  
+
           <Stack.Screen
             name="Replies"
             component={Replies}
             options={{ headerShown: false }}
           />
-		  
-		  <Stack.Screen
+
+          <Stack.Screen
             name="Report"
             component={Report}
             options={{ headerShown: false }}
           />
 
+          <Stack.Screen
+            name="Chat"
+            component={Chat}
+            options={{ headerShown: false }}
+          />
+
+          <Stack.Screen
+            name="Account"
+            component={Account}
+            options={{ headerShown: false }}
+          />
+
+          <Stack.Screen
+            name="Notifications"
+            component={Notifications}
+            options={{ headerShown: false }}
+          />
+
+          <Stack.Screen
+            name="Bug"
+            component={Bug}
+            options={{ headerShown: false }}
+          />
+
+          <Stack.Screen
+            name="Contact"
+            component={Contact}
+            options={{ headerShown: false }}
+          />
+
+          <Stack.Screen
+            name="DeleteAcc"
+            component={DeleteAcc}
+            options={{ headerShown: false }}
+          />
+
+          <Stack.Screen
+            name="RestorPass"
+            component={RestorPass}
+            options={{ headerShown: false }}
+          />
         </Stack.Navigator>
       </NavigationContainer>
     </FontContext.Provider>
